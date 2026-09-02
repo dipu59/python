d2b2243512d7e9f16739f44a07c5000c7d066651
Dipu Biswas <12biswasdipu@gmail.com>
2026-09-03 03:37:18 +0530

    Python Hello World Print with JEE Prep

---
+++ 01_Basics/chai.py
@@ -0,0 +1,3 @@
+from hello_chai import chai
+
+chai("Now time is 3:24 am 03-september-2026")
---
+++ 01_Basics/hello_chai.py
@@ -0,0 +1,7 @@
+print("Chai aur Python")
+
+def chai(n):
+    print(n)
+
+chai("Lemon Tea")
+print(9+22)
